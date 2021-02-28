486e79a259f23ff8219ea17b1da93781084a9eb3
sara-kouadri <75318605+sara-kouadri@users.noreply.github.com>
2021-02-28 21:48:56 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,109 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Institut de beauté MARAM - Montmagny</title>
+    
+  
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+  
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  </head>
+   <body class="homepage">
+      
+               <nav id="nav">
+                  <ul>
+                    <li ><a href="home.php">Accueil</a></li> 
+                    <li><a href="reservation.php">Réservation</a></li>
+                    <li><a href="photo_gallery_backend.php">Galerie prestation</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                  </ul>
+               </nav>
+          <div >
+              <div id="header">
+                    <div class="inner">
+                        <header>
+                          <h1><a >Salon de beauté MARAM <br></a>Coiffure - Esthétique</h1>
+                       
+                          
+                          
+                        </header>
+                      </div>
+              </div>
+          </div>
+
+        <section >
+          <header>
+                 <div id="mc_embed_signup">
+                    <form action="backend/subscribe_newslatters.php" method="post" id="mc-embedded-subscribe-form" name="    mc-embedded-subscribe-form" class="validate" target="_blank"        novalidate>
+                       <div id="mc_embed_signup_scroll">
+                            <h2>Inscrivez-vous à nos newsletters</h2>
+                             <div class="indicates-required"><span>*</span> Obligatoire</div>
+                             <div class="mc-field-group">
+                              <label for="mce-EMAIL">Saisissez votre adresse email<span >*</span>
+                              </label>
+                              <input type="email" value="" placeholder="Entrer votre email" name="EMAIL" class="required email" id="mce-EMAIL">
+                              </div> 
+                        <div class="clear"><input type="submit" value="Je m'inscris" name="subscribe_nl" id="mc-embedded-subscribe" class="button"></div>
+                        </div>
+                      </form>
+                  </div>
+            </header>
+        </section>                 
+    </div>
+
+      <div id ="discription">
+                 <img src="assets/images/image.jpg" alt="" width="100" height="100">
+                  <h3>Vous cherchez un endroit chaleureux, zen et chic ou résonne les mots <strong>"qualité et détente "</strong></h3>
+                  <p>Maram viens d'ouvrir son premier salon de coiffure après des années d'expérience dans les plus grands salons  qui lui ont enseigné les meilleures techniques et les meilleures tendances.</p>
+                  <p>Elle s'est spécialisée dans tout ce qui est soin, lissage brésilien qui permet de redonner vie à vos cheveux tout en facilitant leur entretien au quotidien.</p>
+                  <p>Maram s'est spécialisée dans le lissage Cystemax pour tout type de cheveux européens, méditerranéens et pour cheveux crépus très frisés. C'est un lissage qui permet de lisser les cheveux intensément tout en conservant souplesse matière.</p>
+                  <p>Elle est aussi coloriste,visagiste et elle vous propose des couleurs sans ammoniac pour vous offrir la meilleure coupe qui va avec votre personnalité et votre nature de cheveux ! 
+                  Maram et son équipe, vous proposent les meilleurs coiffeuses qui sont régulièrement formées aux nouvelles techniques et tendances avec les meilleurs soins pour tout type de cheveux qui redonnent brillance et éclat !</p>
+
+           
+
+       </div>
+          <div class="post">
+           <article >
+                    <h1 style="  text-decoration: underline overline" >Avis des clientes</h1>
+                    <article >
+                        <h3>&#128525 &#128525 &#128525 Très beau salon et accueil chaleureux et qualité prix parfaite et très beau travail</h3>
+                        <p>Nawal C. 08/12/20</p>
+                    </article>
+                    <article >
+                        <h3>&#128522 Je suis partie pour des mèches et couleur je suis sortie trop contente un très beau travail et accueil chaleureux par Maram et nawel &#128139</h3>
+                        <p>Malika C. 06/12/20  </p>
+                    </article>
+                    <article >
+                        <h3>Cela fait 18 ans que je me fait coiffé par Maram je l'ai suivie partout et je continue encore c'est une excellente coiffeuse et mabrouk pour le salon</h3>
+                        <p>Lilly Y. 11/12/20</p>
+                    </article>
+                    <h3>. . .</h3>
+           </div>
+<center>
+<b> <p style="font-size: 20px;font-style: italic; " >Nous localiser</p></b>
+
+<iframe  width="80%" height="100%"   src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d2619.03360076389!2d2.3485482155592914!3d48.97188477929874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x47e6690c775c00df%3A0x8a513e46088dfc3e!2s23%20Rue%20des%20Carri%C3%A8res%2C%2095360%20Montmagny!3m2!1d48.9718848!2d2.3507369!5e0!3m2!1sfr!2sfr!4v1614374081969!5m2!1sfr!2sfr" width="600" height="450" style="border: thick double #32a1ce;" allowfullscreen="" ></iframe></center>
+          <footer id="footer">
+                <!-- Contact -->
+                  <section class="contact">
+                    <header>
+                      <h3>Salon MARAM Coiffure et Esthetique</h3>
+                    </header>
+                    <p>95 Rue Des Carrière , 95360 montmagny</p>
+                    <p>Téléphone: 09 49 19 60 60</p>
+                    <p>Portable:  09 49 19 60 55</p>
+                  </section>
+                <!-- Copyright -->
+                  <div class="copyright">
+                    <ul class="menu">
+                      <li>Institut de beauté Maram &copy; 2020 Tous droits réservés </li>
+                      <li>Réalisé par @Sarra</li>
+                    </ul>
+                  </div>
+           </footer>
+
+  </body>
+</html>
+
